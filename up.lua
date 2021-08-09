25870140282e22871641b9fdfdfcb23cb2918482
Cieciak <nikodemrzasa@wp.pl>
2021-08-10 01:14:40 +0200

    Update up.lua

--- up.lua
+++ up.lua
@@ -2,4 +2,11 @@ local up = peripheral.wrap("top")
 
 local width, height = up.getSize()
 
-up.write(width..height)
+
+up.clear()
+up.setCursorPos(1,1)
+
+for i = 0, width do
+    up.setBacgroundColor(i)
+    up.wirte(" ")
+end
