686aeb85e3060f548fa9ed79b56da13e466e0e52
Cieciak <nikodemrzasa@wp.pl>
2021-08-10 01:25:43 +0200

    Update up.lua

--- up.lua
+++ up.lua
@@ -2,11 +2,20 @@ local up = peripheral.wrap("top")
 
 local width, height = up.getSize()
 
+local function wrapLine(monitor)
+    local width, _ = monitor.getSize()
+    local x, y = monitor.getCursorPos()
+    if x >= width then
+        monitor.setCursorPos(1, y + 1)
+    end
+end
 
 up.clear()
 up.setCursorPos(1,1)
 
-for i = 0, width do
-    up.setBacgroundColor(i)
-    up.wirte(" ")
+local j = 1
+for i = 1, 16 do
+    up.setBackgroundColor(j)
+    j = j * 2
+    wrapLine(up)
 end
