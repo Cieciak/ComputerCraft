cde1f4d3244d92e72caa9226aacd79cd29f38f3b
Cieciak <nikodemrzasa@wp.pl>
2021-08-10 03:13:06 +0200

    Update up.lua

--- up.lua
+++ up.lua
@@ -4,24 +4,7 @@ local width, height = up.getSize()
 
 local FILE_NAME = "/computercraft/test.ntxt"
 
-local COLOR_CODES = {
-    "0" = 1,
-    "1" = 2,
-    "2" = 4,
-    "3" = 8,
-    "4" = 16,
-    "5" = 32,
-    "6" = 64,
-    "7" = 128,
-    "8" = 256,
-    "9" = 512,
-    "A" = 1024,
-    "B" = 2048,
-    "C" = 4096,
-    "D" = 8192,
-    "E" = 16384,
-    "F" = 32768,
-}
+local COLOR_CODES = {"0" = 1,"1" = 2,"2" = 4,"3" = 8,"4" = 16,"5" = 32,"6" = 64,"7" = 128,"8" = 256,"9" = 512,"A" = 1024,"B" = 2048,"C" = 4096,"D" = 8192,"E" = 16384,"F" = 32768,}
 
 local function wrapLine(monitor)
     local width, _ = monitor.getSize()
