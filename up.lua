30811c75354cc282cf3691ad98010d013277c464
Cieciak <nikodemrzasa@wp.pl>
2021-08-10 03:10:49 +0200

    Update up.lua

--- up.lua
+++ up.lua
@@ -4,6 +4,25 @@ local width, height = up.getSize()
 
 local FILE_NAME = "/computercraft/test.ntxt"
 
+local COLOR_CODES = {
+    "0" = 1,
+    "1" = 2,
+    "2" = 4,
+    "3" = 8,
+    "4" = 16,
+    "5" = 32,
+    "6" = 64,
+    "7" = 128,
+    "8" = 256,
+    "9" = 512,
+    "A" = 1024,
+    "B" = 2048,
+    "C" = 4096,
+    "D" = 8192,
+    "E" = 16384,
+    "F" = 32768,
+}
+
 local function wrapLine(monitor)
     local width, _ = monitor.getSize()
     local x, y = monitor.getCursorPos()
@@ -34,6 +53,12 @@ local function printLine(monitor, text)
     monitor.setCursorPos(1, y + 1)
 end
 
+local function splitOnNumber(text, index)
+    for i = 1, #text do
+        print(text.i)
+    end
+end
+
 up.clear()
 up.setCursorPos(1,1)
 
@@ -42,3 +67,5 @@ local file = readFile(fs.open(FILE_NAME, "r"))
 for i = 1, #file do
     printLine(up, file.i)
 end
+
+splitOnNumber("Hello World!", 2)
