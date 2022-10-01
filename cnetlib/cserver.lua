e87e7bf6728d39b9dd38119cf1789d77431750ed
Cieciak <nikodemrzasa@wp.pl>
2022-10-01 22:29:28 +0200

    add: pkg test

---
+++ cnetlib/cserver.lua
@@ -0,0 +1,30 @@
+require("csocket")
+
+CServer = {
+    socket = nil,
+    sender_coroutine = nil,
+    reciver_coroutine = nil,
+    name = "",
+}
+
+local function sender()
+    while true do
+        print("Waiting for event")
+    end
+end
+
+function CServer:new(port, recv, name)
+    local o = {}
+    setmetatable(o, self)
+    self.__index = self
+    local s = CSocket.new()
+    o.socket = s
+    o.name = name
+    o.socket:bind(port, recv, true, true)
+    return o
+end
+
+function CServer:start()
+    self.sender_coroutine = coroutine.create(sender)
+    coroutine.resume(self.sender_coroutine)
+end
---
+++ loader/pkg.lua
@@ -0,0 +1,17 @@
+local option, path_to_files = arg
+
+if option == "build" then
+    -- Check for /pkgs
+    if fs.isDir("/pkgs") then print("/pkgs already exist")
+    else fs.makeDir("/pkgs")
+    end
+elseif option == "install" then
+    local cfg_path = fs.combine(path_to_files, 'cfg')
+    print(cfg_path)
+    if cfg_path then
+        local cfg_file = fs.open(cfg_path, "r")
+        local pkg_name = cfg_file.readLine()
+        print(pkg_name)
+    else error("There is no package")
+    end
+end
