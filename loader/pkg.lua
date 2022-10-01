8b14db693b6f69f897ba918bfcc3b9830cbf96cf
Cieciak <nikodemrzasa@wp.pl>
2022-10-01 23:10:11 +0200

    add: pkg test 4

--- loader/pkg.lua
+++ loader/pkg.lua
@@ -10,11 +10,17 @@ if option == "build" then
     end
 elseif option == "install" then
     local cfg_path = fs.combine(path_to_files, 'cfg')
-    print(cfg_path)
     if cfg_path then
         local cfg_file = fs.open(cfg_path, "r")
         local pkg_name = cfg_file.readLine()
-        print(pkg_name)
+        local pkg_path = fs.combine("/pkgs", pkg_name)
+
+        fs.copy(path_to_files, pkg_path)
+
+        local copied_files = fs.list(pkg_path)
+        for name in copied_files do
+            print("Copied ".. name)
+        end
     else error("There is no package")
     end
 end
