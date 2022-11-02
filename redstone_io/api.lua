f001bf156cd84acc53c5e2b133120f297c44b0ec
Cieciak <nikodemrzasa@wp.pl>
2022-11-02 23:15:28 +0100

    fix: logic functions1

--- redstone_io/api.lua
+++ redstone_io/api.lua
@@ -55,7 +55,7 @@ function Redstone_IO_Interface:set(name, flag)
     local color = self.io_map.mapping[name]
     local hex_color = COLOR_MAP[color]
 
-    if flag then
+    if flag == 1 then
         self.state = bit.bor(self.state, hex_color)
     else
         self.state = bit.band(self.state, bit.bnot(hex_color))
